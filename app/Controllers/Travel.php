563f5ca6f85a36ac7157f12cfaff6af63d90591c
minyitan <1913343670@qq.com>
2020-11-11 09:09:15 +0800

    change the Travel.php

--- app/Controllers/Travel.php
+++ app/Controllers/Travel.php
@@ -17,25 +17,57 @@ class Travel  extends BaseController{
     //put your code here
     public function  index() {
         // connect to the model
-       $places = new \App\Models\Places();
+       //$places = new \App\Models\Places();
         // retrieve all the records
-       $records = $places->findAll();
+       //$records = $places->findAll();
        
-       $parser= \Config\Services::parser();
-       return $parser ->setData(['records' => $records])
-               ->render('placeslist');
+       $table = new \CodeIgniter\View\Table();
+       
+       $headings = $places->fields;
+       $displayHeadings = array_slice($headings, 1, 2);
+        $table->setHeading(array_map('ucfirst', $displayHeadings));
+       foreach ($records as $record) {
+       $table->addRow($record->name,$record->description);
+       return $table->generate();
+}
+
+      // $parser= \Config\Services::parser();
+      // return $parser ->setData(['records' => $records])
+        //       ->render('placeslist');
     }
     public function showme($id){
     
         //connect to the model
-        $places= new \App\Models\Places();
+       // $places= new \App\Models\Places();
         //retrieve all the records
-        $record=$places->find($id);
+       // $record=$places->find($id);
         //get a template parser
-        $parser= \Config\Services::parser();
+       // $parser= \Config\Services::parser();
         //tell it about the substitions
-        return $parser->setData($record)
+        //return $parser->setData($record)
                 //and have it render the template with those
-                ->render('oneplace');
+               // ->render('oneplace');
+        
+        $nameLink = anchor("travel/showme/$record->id",$record->name);
+        $table->addRow($nameLink,$record->description);
+        $template = [
+        'table_open' => '<table cellpadding="5px">',
+        'cell_start' => '<td style="border: 1px solid #dddddd;">',
+        'row_alt_start' => '<tr style="background-color:#dddddd">',
+        ];
+        $table->setTemplate($template);
+        
+        $fields = [
+            'title' => 'Travel Destinations',
+             'heading' => 'Travel Destinations',
+             'footer' => 'Copyright Xavier'
+         ];
+        
+        return $parser->setData($fields)
+                      ->render('templates\top') .
+               $table->generate() .
+               $parser->setData($fields)
+                      ->render('templates\bottom');
+
     }
 }
